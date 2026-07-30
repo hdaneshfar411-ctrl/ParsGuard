d24f1fd89d191a43a3626064cb220dbaa9976378
hdaneshfar411-ctrl <hdaneshfar411@gmail.com>
2026-07-31 00:30:57 +0330

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,52 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+
 
 app = FastAPI(title="ParsGuard")
+
 
 templates = Jinja2Templates(directory="backend/templates")
 
 
 @app.get("/")
 def home():
-    return {"message": "ParsGuard is running!"}
+    return {
+        "message": "ParsGuard is running!"
+    }
 
 
 @app.get("/login/")
 def login_page(request: Request):
     return templates.TemplateResponse(
-        request=request,
-        name="login.html",
-        context={}
+        "login.html",
+        {
+            "request": request
+        }
     )
 
 
-from fastapi.responses import RedirectResponse
-
-
 @app.post("/login/")
-def login(username: str = Form(...), password: str = Form(...)):
+def login(
+    username: str = Form(...),
+    password: str = Form(...)
+):
     if username == "admin" and password == "admin":
-        return RedirectResponse("/panel/", status_code=303)
+        return RedirectResponse(
+            "/panel/",
+            status_code=303
+        )
 
-    return {"message": "Wrong username or password"}
+    return {
+        "message": "Wrong username or password"
+    }
+
+
+@app.get("/panel/")
+def panel(request: Request):
+    return templates.TemplateResponse(
+        "panel.html",
+        {
+            "request": request
+        }
+    )
