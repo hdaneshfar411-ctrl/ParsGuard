3c049c61bcfb3836d85ea1e3d5ad70a381c00da1
hdaneshfar411-ctrl <hdaneshfar411@gmail.com>
2026-07-31 00:00:08 +0330

    add login route

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="ParsGuard")
+
+templates = Jinja2Templates(
+    directory="backend/templates"
+)
+
 
 @app.get("/")
 def home():
     return {
         "message": "ParsGuard is running!"
     }
+
+
+@app.get("/login/")
+def login(request: Request):
+    return templates.TemplateResponse(
+        "login.html",
+        {
+            "request": request
+        }
+    )
