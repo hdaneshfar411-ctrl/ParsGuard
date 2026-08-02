270f45688f3d96f4f1f2d7b454130bf8b3ee296b
hesamdeveloper <hdaneshfar411@gmail.com>
2026-08-02 20:46:15 +0330

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -12,12 +12,14 @@ app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 templates = Jinja2Templates(directory="backend/templates")
 
 
+# ---------------- Home ----------------
+
 @app.get("/")
 def home():
-    return {"message": "ParsGuard is running!"}
+    return RedirectResponse(url="/login/")
 
 
-# ---------------- Login Page ----------------
+# ---------------- Login ----------------
 
 @app.get("/login/")
 def login_page(request: Request):
@@ -32,19 +34,78 @@ def login(
     username: str = Form(...),
     password: str = Form(...)
 ):
+
     if username == "admin" and password == "admin":
-        return RedirectResponse("/panel/", status_code=303)
+        return RedirectResponse(
+            url="/panel/",
+            status_code=303
+        )
 
-    return {"message": "Wrong username or password"}
+    return RedirectResponse(
+        url="/login/",
+        status_code=303
+    )
 
 
-# ---------------- Panel ----------------
+# ---------------- Dashboard ----------------
 
 @app.get("/panel/")
 def panel(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="panel.html",
+        context={
+            "username": "Admin"
+        }
+    )
+
+
+# ---------------- Users ----------------
+
+@app.get("/users/")
+def users(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="users.html",
+        context={
+            "username": "Admin"
+        }
+    )
+
+
+# ---------------- Servers ----------------
+
+@app.get("/servers/")
+def servers(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="servers.html",
+        context={
+            "username": "Admin"
+        }
+    )
+
+
+# ---------------- Logs ----------------
+
+@app.get("/logs/")
+def logs(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="logs.html",
+        context={
+            "username": "Admin"
+        }
+    )
+
+
+# ---------------- Settings ----------------
+
+@app.get("/settings/")
+def settings(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="settings.html",
         context={
             "username": "Admin"
         }
